f09aaca563cfdde2d7e4c5bac9c017d33003add2
Mark <kalmark@mail.ru>
2022-10-07 22:29:01 +0300

    Add Ex_9

---
+++ Ex_9/Program.cs
@@ -0,0 +1,7 @@
+﻿int noun = new Random().Next(10, 100);
+Console.WriteLine("Случайное число от 10 до 99: " + noun);
+int first = noun / 10;
+int second = noun % 10;
+Console.Write("Наибольшая цифра: ");
+if (first > second) Console.WriteLine(first);
+else Console.WriteLine(second);
