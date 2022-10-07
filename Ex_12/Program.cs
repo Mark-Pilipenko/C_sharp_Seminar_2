7d0e43ddac7e5812d32049fff31bdc599e85a1f9
Mark <kalmark@mail.ru>
2022-10-07 23:09:33 +0300

    Add Ex_12

---
+++ Ex_12/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите первое число: ");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int second = Convert.ToInt32(Console.ReadLine());
+int rem = first % second;
+if (rem == 0) Console.WriteLine("Первое число кратно второму.");
+else
+{
+    Console.WriteLine("Первое число не кратно второму.");
+    Console.WriteLine("Остаток от деления равен "+ rem);
+}
