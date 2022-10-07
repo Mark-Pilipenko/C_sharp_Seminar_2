96571d7128d16470f604fe1f87d7fd047cbeffe3
Mark <kalmark@mail.ru>
2022-10-07 22:57:57 +0300

    Add Ex_11

---
+++ Ex_11/Program.cs
@@ -0,0 +1,6 @@
+﻿int noun = new Random().Next(100, 1000);
+Console.WriteLine("Случайное трехзначное число: " + noun);
+int first = noun / 100;
+int third = noun % 10;
+int newNoun = first*10 + third;
+Console.WriteLine("Новое двухзначное число: " + newNoun);
