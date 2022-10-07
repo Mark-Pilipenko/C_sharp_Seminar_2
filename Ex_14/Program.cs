df76cbc3c685f63a35e5468c2e2b6703b6d8d3a2
Mark <kalmark@mail.ru>
2022-10-07 23:34:57 +0300

    Add Ex_14 Ex_16

---
+++ Ex_14/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите число: ");
+int noun = Convert.ToInt32(Console.ReadLine());
+if (noun % 7 == 0 && noun % 23 == 0) Console.WriteLine("Число кратно 7 и 23.");
+else Console.WriteLine("Число не кратно 7 и 23.");
---
+++ Ex_16/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите первое число: ");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int second = Convert.ToInt32(Console.ReadLine());
+if (first*first == second | second*second == first) Console.WriteLine("YES!");
+else Console.WriteLine("NO");
